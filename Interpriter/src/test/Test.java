1b6f49c5e09b893fb511e7b129a42609122245e9
JesusWasHomeless <48917732+JesusWasHomeless@users.noreply.github.com>
2019-05-16 00:28:06 +0300

    Update Test.java

--- Interpriter/src/test/Test.java
+++ Interpriter/src/test/Test.java
@@ -1,38 +1,26 @@
 package test;
 
-//import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-//import java.lang.reflect.Array;
-//import java.util.ArrayList;
 import java.util.LinkedList;
-//import java.util.List;
-//import java.util.Scanner;
-//import java.util.Queue;
 
 import lexer.LexGrammar;
 import lexer.Lexer;
 import lexer.Token;
 import parser.Parser;
+import poliz.Poliz;
+import stackMachine.StackMachine;
+
 
 public class Test {
-/*	public static String FileReader(String phrase) throws Exception{
-		FileReader file = new FileReader("Test.txt");
-		Scanner scan = new Scanner(file);
-		while(scan.hasNextLine()) {
-			phrase = scan.nextLine();
-		}
-		file.close();
-		scan.close();
-		return phrase;
-	}
-	*/
 	public static void main(String[] args) throws Exception{
-		LinkedList <Token> tokens = new LinkedList<>();
-		Lexer lexer = new Lexer();
+		LinkedList <Token> tokens;
+        LinkedList <Token> tokens2;
+        Lexer lexer = new Lexer();
 		//String string = null;
 		String stroka = new String(Files.readAllBytes(Paths.get("test.txt")));
-		tokens = lexer.tokenize(stroka);
+		tokens = lexer.tokenize(stroka);//на вход полизу приходят токены, полученные из файла
+        tokens2 = tokens;
 		for (Token token : tokens){
 			LexGrammar lex = token.getLex();
 			String val = token.getVal();
@@ -40,9 +28,14 @@ public class Test {
 			System.out.println("The lexeme: {" + lex + "} stands for: {" + val + "}");
 		}
 		System.out.println("\n");
-		new Parser(tokens);
-		System.out.println("\n");
+		Parser parser = new Parser(tokens);
+        System.out.println("\n");
 		System.out.println(stroka);
-		System.out.println(Parser.Poliz(tokens));
-   }
-}
+		System.out.println(parser.tokens);
+        System.out.println("\n");
+        StackMachine SM = new StackMachine();
+        SM.initialization(tokens2);
+        Poliz poliz = new Poliz();
+        LinkedList<Token> pol =  poliz.Poliz(tokens);
+        SM.interpreter(pol);
+    }
